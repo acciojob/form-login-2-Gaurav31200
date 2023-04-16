258110fd2670d99a2dbc09f28b7d69d7b85a31f8
Gaurav Saini <81917811+Gaurav31200@users.noreply.github.com>
2023-04-16 11:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const submitHandler = ()=>{
+	let inputs = document.getElementsByTagName("input");
+	alert("First Name: " + inputs[0].value
+		   + "\nLast Name: " + inputs[1].value
+		   + "\nPhone Number: " + inputs[2].value
+		   + "\nEmail ID: " + inputs[3].value);
+}
